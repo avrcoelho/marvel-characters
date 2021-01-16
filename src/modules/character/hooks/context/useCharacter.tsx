75387d36a6969c4a991a0746dc738a181ed88801
avrcoelho <andrevrcoelho@hotmail.com>
2021-01-16 12:40:33 -0300

    feat: contex hook useCharacter

---
+++ src/modules/character/hooks/context/useCharacter.tsx
@@ -0,0 +1,40 @@
+import { createContext, useState, useContext } from 'react';
+
+import { CharacterDataContainerModel } from '../../models/Character.model';
+import { getCharactersService } from '../../services';
+
+interface CharacterContextData {
+  characters: CharacterDataContainerModel | null;
+  getCharacters(serach: string): Promise<void>;
+}
+
+const CharacterContext = createContext<CharacterContextData>(
+  {} as CharacterContextData,
+);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [
+    characters,
+    setCharacters,
+  ] = useState<CharacterDataContainerModel | null>(null);
+
+  const getCharacters = async (search = ''): Promise<void> => {
+    const response = await getCharactersService.execute(search);
+
+    if (response) {
+      setCharacters(response);
+    }
+  };
+
+  return (
+    <CharacterContext.Provider value={{ characters, getCharacters }}>
+      {children}
+    </CharacterContext.Provider>
+  );
+};
+
+export function useCharacter(): CharacterContextData {
+  const context = useContext(CharacterContext);
+
+  return context;
+}
