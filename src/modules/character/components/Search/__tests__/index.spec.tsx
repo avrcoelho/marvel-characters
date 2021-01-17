57713f8877a9038323fb8a1307d93956d887cfe4
avrcoelho <andrevrcoelho@hotmail.com>
2021-01-17 14:54:39 -0300

    chore: teste and refacted component search

---
+++ src/modules/character/components/Search/__tests__/index.spec.tsx
@@ -0,0 +1,51 @@
+import { screen, render, fireEvent, act } from '@testing-library/react';
+
+import Search from '..';
+
+const mockGetCharacters = jest.fn();
+
+jest.mock('../../../hooks/context/character', () => {
+  return {
+    useCharacter: () => ({
+      getCharacters: mockGetCharacters,
+    }),
+  };
+});
+
+const runInterval = async (): Promise<any> => {
+  await new Promise(r => setTimeout(r, 500));
+};
+
+describe('Search', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  it('should be able to search characters', () => {
+    render(<Search />);
+    const inputElement = screen.getByRole('textbox');
+
+    act(() => {
+      fireEvent.change(inputElement, { target: { value: 'test' } });
+
+      jest.advanceTimersByTime(500);
+      runInterval();
+    });
+
+    expect(mockGetCharacters).toHaveBeenCalledWith('test');
+  });
+
+  it('should not be able to search characters', () => {
+    render(<Search />);
+    const inputElement = screen.getByRole('textbox');
+
+    act(() => {
+      fireEvent.change(inputElement, { target: { value: '' } });
+
+      jest.advanceTimersByTime(500);
+      runInterval();
+    });
+
+    expect(mockGetCharacters).not.toHaveBeenCalled();
+  });
+});
--- src/modules/character/components/Search/index.tsx
+++ src/modules/character/components/Search/index.tsx
@@ -1,7 +1,32 @@
-import { Container } from './styles';
+import { useEffect, useState } from 'react';
+
 import { ReactComponent as SearchIcon } from '../../../../assets/svgs/search.svg';
+import { useDebounce } from '../../../../shared/hooks/useDebounce';
+import { useCharacter } from '../../hooks/context/character';
+
+import { Container } from './styles';
 
 const Search = (): JSX.Element => {
+  const [searchValue, setSearchValue] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const debouncedSearchValue = useDebounce({ value: searchValue, delay: 500 });
+  const { getCharacters } = useCharacter();
+
+  useEffect(() => {
+    async function search(): Promise<void> {
+      if (debouncedSearchValue) {
+        setIsLoading(true);
+
+        await getCharacters(debouncedSearchValue);
+
+        setIsLoading(false);
+      }
+    }
+
+    search();
+  }, [debouncedSearchValue, getCharacters]);
+
   return (
     <Container>
       <SearchIcon />
@@ -11,6 +36,8 @@ const Search = (): JSX.Element => {
         name="search"
         placeholder="Procure por herois"
         autoComplete="off"
+        value={searchValue}
+        onChange={event => setSearchValue(event.target.value)}
       />
     </Container>
   );
