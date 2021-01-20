5e9c85101af8ae8634f70f8a0a930ad999667d26
avrcoelho <andrevrcoelho@hotmail.com>
2021-01-20 19:30:01 -0300

    refactor: function favorite ou remove

--- src/modules/character/components/Buttons/Favorite/__tests__/index.spec.tsx
+++ src/modules/character/components/Buttons/Favorite/__tests__/index.spec.tsx
@@ -2,31 +2,63 @@ import { render, screen, fireEvent } from '@testing-library/react';
 
 import Favorite from '..';
 
-const mockOnClick = jest.fn();
+const mockSaveFavorite = jest.fn();
+const mockRemoveFavorite = jest.fn();
+
+jest.mock('../../../../hooks/context/character', () => {
+  return {
+    useCharacter: () => ({
+      saveFavorite: mockSaveFavorite,
+      removeFavorite: mockRemoveFavorite,
+    }),
+  };
+});
 
 describe('Button favorite', () => {
   const props = {
-    isFavorite: true,
-    onClick: mockOnClick,
+    character: {
+      id: 1,
+      name: 'John Doe',
+      thumbnail: {
+        path: 'path',
+        extension: 'jpg',
+      },
+      comics: {
+        returned: 3,
+      },
+      description: 'description',
+      isFavorite: false,
+    },
   };
 
-  it('should be able to remove of favorites', () => {
+  it('should be able to add character on the favorites', () => {
     render(<Favorite {...props} />);
     const buttonElement = screen.getByRole('button');
 
     fireEvent.click(buttonElement);
 
-    expect(mockOnClick).toHaveBeenCalled();
+    expect(mockSaveFavorite).toHaveBeenCalled();
+  });
+
+  it('should be able to remove character on the favorites', () => {
+    Object.assign(props.character, { isFavorite: true });
+    render(<Favorite {...props} />);
+    const buttonElement = screen.getByRole('button');
+
+    fireEvent.click(buttonElement);
+
+    expect(mockRemoveFavorite).toHaveBeenCalled();
   });
 
   it('should be able to render hearth border icon', () => {
+    Object.assign(props.character, { isFavorite: true });
     render(<Favorite {...props} />);
 
     expect(screen.getByTestId('hearth')).toBeTruthy();
   });
 
   it('should be able to render hearth icon', () => {
-    Object.assign(props, { isFavorite: false });
+    Object.assign(props.character, { isFavorite: false });
     render(<Favorite {...props} />);
 
     expect(screen.getByTestId('hearth-border')).toBeTruthy();
--- src/modules/character/components/Buttons/Favorite/index.tsx
+++ src/modules/character/components/Buttons/Favorite/index.tsx
@@ -1,19 +1,38 @@
-import { ButtonHTMLAttributes } from 'react';
+import { useCallback } from 'react';
+
+import { CharacterModel } from '../../../models/Character.model';
+import { useCharacter } from '../../../hooks/context/character';
 
 import { ReactComponent as Hearth } from '../../../../../assets/svgs/hearth-sm.svg';
 import { ReactComponent as HearthBorder } from '../../../../../assets/svgs/hearth-border-sm.svg';
 import { Container } from './styles';
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement> & { isFavorite: boolean };
+interface Props {
+  character: CharacterModel;
+}
 
-const Favorite = ({ isFavorite, ...rest }: Props): JSX.Element => (
-  <Container type="button" {...rest}>
-    {isFavorite ? (
-      <Hearth data-testid="hearth" />
-    ) : (
-      <HearthBorder data-testid="hearth-border" />
-    )}
-  </Container>
-);
+const Favorite = ({ character }: Props): JSX.Element => {
+  const { removeFavorite, saveFavorite } = useCharacter();
+
+  const addOrRemoveFavorite = useCallback((): void => {
+    if (character.isFavorite) {
+      removeFavorite(character.id);
+
+      return;
+    }
+
+    saveFavorite(character);
+  }, [character, removeFavorite, saveFavorite]);
+
+  return (
+    <Container type="button" onClick={addOrRemoveFavorite}>
+      {character.isFavorite ? (
+        <Hearth data-testid="hearth" />
+      ) : (
+        <HearthBorder data-testid="hearth-border" />
+      )}
+    </Container>
+  );
+};
 
 export default Favorite;
