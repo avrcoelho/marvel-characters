3bd2f32e57224e581e413bf4a06276a1e81f18a8
avrcoelho <andrevrcoelho@hotmail.com>
2021-01-21 14:35:59 -0300

    chore: setup file test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,3 @@
 process.env.REACT_APP_API = 'http://localhost:3333';
-process.env.REACT_APP_API_PUBLIC_KEY = '727549817b57a494cec3c766b713f24d';
-process.env.REACT_APP_API_PRIVATE_KEY =
-  'f1c69dd1e13f0f46f0f3061f4718c0d450a6e682';
+process.env.REACT_APP_API_PUBLIC_KEY = 'public';
+process.env.REACT_APP_API_PRIVATE_KEY = 'private';
