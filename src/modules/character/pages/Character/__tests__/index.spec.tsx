537359956d8f5be4340979e7da228a0156da728d
avrcoelho <andrevrcoelho@hotmail.com>
2021-01-22 09:26:06 -0300

    refactor: add character details hook

--- src/modules/character/pages/Character/__tests__/index.spec.tsx
+++ src/modules/character/pages/Character/__tests__/index.spec.tsx
@@ -1,34 +1,43 @@
-import {
-  act,
-  render,
-  waitFor,
-  screen,
-  fireEvent,
-} from '@testing-library/react';
-import { toast } from 'react-toastify';
+import { act, render, screen, fireEvent } from '@testing-library/react';
 
-import {
-  getCharacterService,
-  getCharacterComicsService,
-  getFavoritesCharactersService,
-} from '../../../services';
 import Character from '..';
 
 const promise = Promise.resolve();
 
-const mockSaveFavorite = jest.fn();
-const mockRemoveFavorite = jest.fn();
+const mockGetCharacterDetails = jest.fn();
+const mockAddOrRemoveFavorite = jest.fn();
+
+const characterDetails = {
+  id: 1,
+  name: 'John Doe',
+  thumbnail: {
+    path: 'path',
+    extension: 'jpg',
+  },
+  comics: {
+    returned: 3,
+  },
+  series: {
+    returned: 3,
+  },
+  description: 'description',
+  isFavorite: false,
+};
+
+const comics: any[] = [];
 
 jest.mock('react-router-dom', () => {
   return {
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
-jest.mock('../../../hooks/context/character', () => {
+jest.mock('../../../hooks/characterDetails', () => {
   return {
-    useCharacter: () => ({
-      saveFavorite: mockSaveFavorite,
-      removeFavorite: mockRemoveFavorite,
+    useCharacterDetails: () => ({
+      getCharacterDetails: mockGetCharacterDetails,
+      addOrRemoveFavorite: mockAddOrRemoveFavorite,
+      characterDetails,
+      comics,
     }),
   };
 });
@@ -41,156 +50,19 @@ jest.mock('react-router-dom', () => {
 });
 
 describe('Character page', () => {
-  const responseCharacter = {
-    offset: 0,
-    limit: 20,
-    total: 1493,
-    count: 20,
-    results: [
-      {
-        id: 1,
-        name: 'John Doe',
-        thumbnail: {
-          path: 'path',
-          extension: 'jpg',
-        },
-        comics: {
-          returned: 3,
-        },
-        series: {
-          returned: 3,
-        },
-        description: 'description',
-        isFavorite: false,
-      },
-    ],
-  };
-  const responseComic = {
-    offset: 0,
-    limit: 20,
-    total: 1493,
-    count: 20,
-    results: [],
-  };
   it('should be able to get character', async () => {
-    const spyGetCharacterService = jest
-      .spyOn(getCharacterService, 'execute')
-      .mockImplementation((): any => {
-        return responseCharacter;
-      });
-    jest
-      .spyOn(getCharacterComicsService, 'execute')
-      .mockImplementation((): any => {
-        return responseComic;
-      });
-    jest
-      .spyOn(getFavoritesCharactersService, 'execute')
-      .mockImplementation((): any => {
-        return {
-          results: responseCharacter.results,
-        };
-      });
     render(<Character />);
 
-    expect(spyGetCharacterService).toHaveBeenCalledWith('123');
-    await act(() => promise);
+    expect(mockGetCharacterDetails).toHaveBeenCalledWith('123');
   });
 
-  it('should be able to get comic', async () => {
-    jest.spyOn(getCharacterService, 'execute').mockImplementation((): any => {
-      return responseCharacter;
-    });
-    const spyGetCharacterComicsService = jest
-      .spyOn(getCharacterComicsService, 'execute')
-      .mockImplementation((): any => {
-        return responseComic;
-      });
-    jest
-      .spyOn(getFavoritesCharactersService, 'execute')
-      .mockImplementation((): any => {
-        return {
-          results: responseCharacter.results,
-        };
-      });
-    render(<Character />);
-
-    expect(spyGetCharacterComicsService).toHaveBeenCalledWith('123');
-    await act(() => promise);
-  });
-
-  it('should be able to show alert', async () => {
-    jest
-      .spyOn(getCharacterComicsService, 'execute')
-      .mockImplementation((): any => {
-        throw new Error();
-      });
-    jest
-      .spyOn(getCharacterComicsService, 'execute')
-      .mockImplementation((): any => {
-        throw new Error();
-      });
-    jest
-      .spyOn(getFavoritesCharactersService, 'execute')
-      .mockImplementation((): any => {
-        return {
-          results: responseCharacter.results,
-        };
-      });
-    const spyToast = jest.spyOn(toast, 'error');
-    render(<Character />);
-
-    await waitFor(() => {
-      expect(spyToast).toHaveBeenCalled();
-    });
-  });
-
-  it('should be able to add character on the favorites', async () => {
-    jest.spyOn(getCharacterService, 'execute').mockImplementation((): any => {
-      return responseCharacter;
-    });
-    jest
-      .spyOn(getCharacterComicsService, 'execute')
-      .mockImplementation((): any => {
-        return responseComic;
-      });
-    jest
-      .spyOn(getFavoritesCharactersService, 'execute')
-      .mockImplementation((): any => {
-        return {
-          results: [{ ...responseCharacter.results[0], id: 11 }],
-        };
-      });
+  it('should be able to add or remove character on favorites', async () => {
     render(<Character />);
     await act(() => promise);
     const buttonElement = screen.getByRole('button');
 
     fireEvent.click(buttonElement);
 
-    expect(mockSaveFavorite).toHaveBeenCalled();
-  });
-
-  it('should be able to remove character on the favorites', async () => {
-    jest.spyOn(getCharacterService, 'execute').mockImplementation((): any => {
-      return responseCharacter;
-    });
-    jest
-      .spyOn(getCharacterComicsService, 'execute')
-      .mockImplementation((): any => {
-        return responseComic;
-      });
-    jest
-      .spyOn(getFavoritesCharactersService, 'execute')
-      .mockImplementation((): any => {
-        return {
-          results: responseCharacter.results,
-        };
-      });
-    render(<Character />);
-    await act(() => promise);
-    const buttonElement = screen.getByRole('button');
-
-    fireEvent.click(buttonElement);
-
-    expect(mockRemoveFavorite).toHaveBeenCalled();
+    expect(mockAddOrRemoveFavorite).toHaveBeenCalled();
   });
 });
--- src/modules/character/pages/Character/index.tsx
+++ src/modules/character/pages/Character/index.tsx
@@ -1,87 +1,31 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 import Header from '../../components/Headers/Character';
 import CharacterDetails from '../../components/CharacterDetails';
 import ListComics from '../../components/Lists/Comics';
-import { CharacterModel } from '../../models/Character.model';
-import { ComicModel } from '../../models/Comic.model';
-import {
-  getCharacterService,
-  getCharacterComicsService,
-  getFavoritesCharactersService,
-} from '../../services';
-import { useCharacter } from '../../hooks/context/character';
+import Footer from '../../components/Footer';
+import { useCharacterDetails } from '../../hooks/characterDetails';
 
 import { Container, MainContainer, CharacterNameBg } from './styles';
-import Footer from '../../components/Footer';
 
 interface ParamsRoute {
   id: string;
 }
 
 const Character = (): JSX.Element => {
-  const [
-    characterDetails,
-    setCharacterDetails,
-  ] = useState<CharacterModel | null>(null);
-  const [comics, setComics] = useState<ComicModel[]>([]);
-  const [dateOfLastComic, setDateOfLastComic] = useState<string | null>(null);
-
   const { id } = useParams<ParamsRoute>();
-  const { saveFavorite, removeFavorite } = useCharacter();
-
-  const verifyIfIsFavorite = useCallback((characterId: number): boolean => {
-    const favorites = getFavoritesCharactersService.execute();
-    const isFavorite = favorites.results.some(
-      favorite => favorite.id === characterId,
-    );
-
-    return isFavorite;
-  }, []);
-
-  const remoteConsultAndSetInState = useCallback(
-    async (characterId: string): Promise<void> => {
-      const [characterResult, comicResult] = await Promise.all([
-        getCharacterService.execute(characterId),
-        getCharacterComicsService.execute(characterId),
-      ]);
-
-      const isFavorite = verifyIfIsFavorite(characterResult.results[0].id);
-
-      setCharacterDetails({ ...characterResult.results[0], isFavorite });
-      setComics(comicResult.results);
-
-      const getDateOfLastComic = comicResult.results[0]?.dates.pop()?.date;
-      setDateOfLastComic(getDateOfLastComic || null);
-    },
-    [verifyIfIsFavorite],
-  );
-
-  const handleAddOrRemoveFavorite = useCallback((): void => {
-    if (characterDetails?.isFavorite) {
-      removeFavorite(Number(characterDetails.id));
-    } else {
-      saveFavorite(characterDetails as CharacterModel);
-    }
-
-    const isFavorite = verifyIfIsFavorite(characterDetails?.id as number);
-
-    setCharacterDetails(prevState => prevState && { ...prevState, isFavorite });
-  }, [characterDetails, removeFavorite, saveFavorite, verifyIfIsFavorite]);
+  const {
+    addOrRemoveFavorite,
+    getCharacterDetails,
+    characterDetails,
+    comics,
+    dateOfLastComic,
+  } = useCharacterDetails();
 
   useEffect(() => {
-    const getCharacterDetails = async (): Promise<void> => {
-      try {
-        await remoteConsultAndSetInState(id);
-      } catch {
-        toast.error('Erro ao obter o personagem');
-      }
-    };
-
-    getCharacterDetails();
-  }, [id, remoteConsultAndSetInState]);
+    getCharacterDetails(id);
+  }, [getCharacterDetails, id]);
 
   return (
     <>
@@ -94,7 +38,7 @@ const Character = (): JSX.Element => {
                 <CharacterDetails
                   character={characterDetails}
                   dateOfLastComic={dateOfLastComic}
-                  handleAddOrRemoveFavorite={handleAddOrRemoveFavorite}
+                  handleAddOrRemoveFavorite={addOrRemoveFavorite}
                 />
 
                 <img
