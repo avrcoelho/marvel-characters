1a8589f897642c45d4d69d9c1cf0b941b0ab906f
André Coelho <andrevrcoelho@hotmail.com>
2021-03-16 21:28:54 -0300

    Update useDebounce.ts

--- src/shared/hooks/useDebounce.ts
+++ src/shared/hooks/useDebounce.ts
@@ -1,13 +1,13 @@
 import { useCallback, useRef } from 'react';
 
-export const useDebounce = (delay: number): ((func: () => void) => void) => {
-  const timer = useRef<ReturnType<typeof setTimeout>>(0);
+let timer: NodeJS.Timeout;
 
+export const useDebounce = (delay: number): ((func: () => void) => void) => {
   const handleDebounce = useCallback(
     (func): void => {
-      clearTimeout(timer.current);
+      clearTimeout(timer);
 
-      timer.current = setTimeout(() => {
+      timer = setTimeout(() => {
         func();
       }, delay);
     },
